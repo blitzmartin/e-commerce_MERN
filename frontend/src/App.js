6cd3d5d4d040d9437b9d1b74344111aedbe44d91
blitzmartin <lizmartinmusic@gmail.com>
2022-06-21 18:29:58 +0200

    added boostrap styling to main footer header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header>
-          <a href="/" target="_blank">
-            amazona
-          </a>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:slug" element={<Products />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:slug" element={<Products />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </Router>
   );
