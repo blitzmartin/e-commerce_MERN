46554d48bde97f45a20b08d77d892da721925ce0
blitzmartin <lizmartinmusic@gmail.com>
2022-06-22 16:28:34 +0200

    implemented single item page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,7 @@ import logger from "use-reducer-logger";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from "../components/Product";
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,13 +21,12 @@ const reducer = (state, action) => {
 };
 
 export default function Home() {
+
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: "",
   });
-
-  // const [ products, setProducts ] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,13 +37,15 @@ export default function Home() {
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
-      // setProducts(result.data);
     };
     fetchData();
   }, []);
 
   return (
     <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
