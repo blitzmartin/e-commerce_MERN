19a20aad3355a243f9db060a81d0ac521ebab95f
blitzmartin <lizmartinmusic@gmail.com>
2022-06-30 19:58:22 +0200

    created user model

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: "Annalisa",
+            email: "admin@example.com",
+            password: bcrypt.hashSync('12345'),
+            isAdmin: true,
+        },
+        {
+            name: "John",
+            email: "user@example.com",
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        }
+    ],
     products: [
         {
             name: 'Nike Slim Shirt',
