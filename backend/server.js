7c73aa9a07b34216803276b046346912ef07d087
blitzmartin <lizmartinmusic@gmail.com>
2022-06-30 18:58:01 +0200

    connected to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_SERVER)
+.then(() => {
+    console.log('Connected to MongoDB Atlas...')
+})
+.catch(err => {
+    console.log(err.message)
+});
 
 const app = express();
 
