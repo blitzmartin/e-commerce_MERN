51671506f4451d7790a1e7e1febf8de812bcf5c5
blitzmartin <lizmartinmusic@gmail.com>
2022-07-04 13:28:24 +0200

    sign in and sign out in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from 'react-bootstrap/Nav';
-import Badge from 'react-bootstrap/Badge';
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Badge from "react-bootstrap/Badge";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
-import { Store } from './Store';
-import Cart from './pages/Cart'
-import SignIn from './pages/SignIn';
+import { Store } from "./Store";
+import Cart from "./pages/Cart";
+import SignIn from "./pages/SignIn";
 
 function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
 
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT '});
+    localStorage.removeItem('userInfo');
+  }
 
   return (
     <Router>
       <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -27,19 +35,41 @@ function App() {
               </LinkContainer>
               <Nav className="mt-auto">
                 <Link to="/cart" className="nav-link">
-                Cart
-                {cart.cartItems.length > 0 && (
-                  <Badge pill bg="danger">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                  </Badge>
-                )}
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer> 
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                    className="dropdown-item"
+                    to="#signout"
+                    onClick={signoutHandler}
+                    >
+                    Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
               </Nav>
             </Container>
-          </Navbar> 
+          </Navbar>
         </header>
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/product/:slug" element={<Products />} />
